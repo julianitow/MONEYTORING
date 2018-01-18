7fb0b3c257d7ac101f8f36c8f930a5ae0db79555
julien-guillan <guillan.julien@live.com>
2018-01-18 18:09:26 +0100

    Formulaire de connexion test

--- src/ApplicationBundle/Controller/DefaultController.php
+++ src/ApplicationBundle/Controller/DefaultController.php
@@ -2,12 +2,40 @@
 
 namespace ApplicationBundle\Controller;
 
+use ApplicationBundle\Entity\Utilisateur;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('@Application/Default/index.html.twig');
+    }
+
+    public function connexionAction(Request $request)
+    {
+        $utilisateur = new Utilisateur();
+
+        $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $utilisateur);
+
+        $formBuilder
+            ->add('email', TextType::class)
+            ->add('motDePasse', TextType::class)
+            ->add('Valider', SubmitType::class);
+
+        $form = $formBuilder->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $utilisateur = $form->getData();
+        }
+
+        return $this->render('@Application/Default/connexion.html.twig', ['form'=> $form->createView(), 'utilisateur'=> $utilisateur]);
     }
 }
