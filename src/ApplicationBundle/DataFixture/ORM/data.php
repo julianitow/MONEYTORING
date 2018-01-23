ea2634e1e1c1c4d32bb17a29b7b474d051c60cda
julien-guillan <guillan.julien@live.com>
2018-01-23 13:58:27 +0100

    Tiens riton

---
+++ src/ApplicationBundle/DataFixture/ORM/data.php
@@ -0,0 +1,21 @@
+<?php
+namespace Application\DataFixtures;
+use ApplicationBundle\Entity\Utilisateur;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+    class ApplicationFixtures extends Fixture
+    {
+        public function load(ObjectManager $manager)
+        {
+            $utilisateur = new Utilisateur();
+            $utilisateur->setNom('Chinnapong');
+            $utilisateur->setPrenom('Didier');
+            $utilisateur->setMotDePasse('couille');
+            $utilisateur->setEmail('didier.chinnapong@gmail.com');
+            $utilisateur->setDateNaiss(new \DateTime('03/10/1997'));
+            $utilisateur->setBudgetGlobal(0);
+            $utilisateur->setDaltonisme(false);
+            $utilisateur->setAdmin(false);
+        }
+    $manager->flush();
+    }
